2f6885d70ea563d4f4e5f18d2f520dc481261d00
marquinhos03 <mliguempibozzano@gmail.com>
2026-07-04 01:59:47 -0400

    refactor: hashing_cerrado es clase template

---
+++ hashing_cerrado.h
@@ -0,0 +1,172 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+enum Estado { VACIO, OCUPADO, BORRADO };
+
+enum ProbingType { LINEAR_PROBING, QUADRATIC_PROBING, DOUBLE_HASHING };
+
+template <typename CType>
+struct Entrada {
+    CType clave;
+    int valor;
+    Estado estado;
+
+    Entrada() : valor(0), estado(VACIO) {}
+};
+
+template <typename CType>
+class TablaHashingCerrado {
+private:
+    std::vector<Entrada<CType>> tabla;
+    int size;
+    ProbingType probing_type;
+
+    int h1(CType k, int n);
+    int h2(CType k, int n);
+
+    int linear_probing(CType k, int n, int i) {
+        return (h1(k, n) + i) % n;
+    }
+
+    int quadratic_probing(CType k, int n, int i) {
+        return (h1(k, n) + i + 2 * i * i) % n;
+    }
+
+    int double_hashing(CType k, int n, int i) {
+        return (h1(k, n) + i * h2(k, n)) % n;
+    }
+
+    int calcularIndiceSondeo(CType clave, int i) {
+        switch (probing_type) {
+            case LINEAR_PROBING: return linear_probing(clave, size, i);
+            case QUADRATIC_PROBING: return quadratic_probing(clave, size, i);
+            case DOUBLE_HASHING: return double_hashing(clave, size, i);
+            default: throw std::invalid_argument("ProbingType (Tipo de sondeo) no reconocido.");
+        }
+    }
+
+public:
+    TablaHashingCerrado(int s, ProbingType pt) : size(s), probing_type(pt) {
+        tabla.resize(size);
+    }
+
+    void insertar(CType clave, int valor) {
+        for (int i = 0; i < size; i++) {
+            int indice = calcularIndiceSondeo(clave, i);
+
+            if (tabla[indice].estado == VACIO || tabla[indice].estado == BORRADO) {
+                tabla[indice].clave = clave;
+                tabla[indice].valor = valor;
+                tabla[indice].estado = OCUPADO;
+                return;
+            }
+
+            // Si ya existe, actualizar
+            if (tabla[indice].estado == OCUPADO && tabla[indice].clave == clave) {
+                tabla[indice].valor = valor;
+                return;
+            }
+        }
+
+        std::cout << "Tabla llena, no se pudo insertar " << clave << std::endl;
+    }
+
+    int get(CType clave) {
+        for (int i = 0; i < size; i++) {
+            int indice = calcularIndiceSondeo(clave, i);
+
+            if (tabla[indice].estado == VACIO) break;   // Ya no va a aparecer
+
+            if (tabla[indice].estado == OCUPADO && tabla[indice].clave == clave) {
+                return tabla[indice].valor;
+            }
+        }
+        return -1;
+    }
+
+    void remover(CType clave) {
+        for (int i = 0; i < size; i++) {
+            int indice = calcularIndiceSondeo(clave, i);
+
+            if (tabla[indice].estado == VACIO) break;
+
+            if (tabla[indice].estado == OCUPADO && tabla[indice].clave == clave) {
+                tabla[indice].estado = BORRADO;
+                return;
+            }
+        }
+    }
+
+    void imprimir() {
+        std::cout << "\n--- Estado actual de la Tabla ---" << std::endl;
+        for (int i = 0; i < size; i++) {
+            std::cout << "[" << i << "] ";
+            
+            if (tabla[i].estado == OCUPADO) {
+                std::cout << "Clave: " << tabla[i].clave << "\t| Valor: " << tabla[i].valor;
+            } else if (tabla[i].estado == VACIO) {
+                std::cout << "VACIO";
+            } else if (tabla[i].estado == BORRADO) {
+                std::cout << "BORRADO";
+            }
+            
+            std::cout << std::endl;
+        }
+
+        std::cout << "---------------------------------" << std::endl;
+    }
+};
+
+
+// --- Función Hash para string (user_screen_name) ---
+
+template <>
+int TablaHashingCerrado<std::string>::h1(std::string k, int n) {
+    long long h = 0;
+    int a = 127;
+
+    for (char v : k) {
+        // Multiplicamos el acumulado, sumamos el ASCII, y aplicamos módulo
+        h = (a * h + v) % n;
+    }
+
+    return h;
+}
+
+template <>
+int TablaHashingCerrado<std::string>::h2(std::string k, int n) {
+    long long h = 0;
+    int a = 31; // Un número primo distinto al 127 de h1
+
+    // Calculamos un hash base distinto
+    for (char v : k) {
+        h = (a * h + v) % n;
+    }
+
+    // Sea R un número primo menor que el tamaño de la tabla "n"
+    // Nota: se puede settear un número primo estático grande o calcular uno dinámicamente.
+    int R = 97; 
+
+    // Aplicamos la fórmula matemática para garantizar que el salto nunca sea 0 para evitar bucles infinitos. Se garantiza que el valor esté en el rango [1, R].    
+    return R - (h % R);
+}
+
+
+
+// --- Función Hash para long long (user_id) ---
+
+template <>
+int TablaHashingCerrado<long long>::h1(long long k, int n) {
+    return k % n;
+}
+
+template <>
+int TablaHashingCerrado<long long>::h2(long long k, int n) {
+    // R debe ser un primo menor que el tamaño de la tabla
+    int R = 97; 
+    return R - (k % R);
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
-#include "hashing_cerrado.hpp"
+// Compilación Linux: g++ main.cpp -o main
+// Ejecución Linux: ./main
+
+#include "hashing_cerrado.h"
 #include <iostream>
 #include "extern/csv.h"
 
@@ -9,12 +12,17 @@ int main() {
     // factor de carga: alfa = n / N
     // En esta prueba, alfa = 0.5 (no cumple alfa < 0.5)
     // Número de pruebas esperado con direccionamiento abierto = 1 / (1 - alfa) = 2
-    TablaHashingCerrado tablaTweets(N, LINEAR_PROBING);
+
+    //TablaHashingCerrado<string> tabla_user_screen_name(N, LINEAR_PROBING);
+    TablaHashingCerrado<long long> tabla_user_id(N, LINEAR_PROBING);
 
     io::CSVReader<1, io::trim_chars<' ', '\t'>, io::double_quote_escape<',', '\"'>> in("datasets/auspol2019.csv");
-    in.read_header(io::ignore_extra_column, "user_screen_name");
 
-    string user_screen_name;
+    //in.read_header(io::ignore_extra_column, "user_screen_name");
+    in.read_header(io::ignore_extra_column, "user_id");
+
+    //string user_screen_name;
+    long long user_id;
     int limiteFilas = 20;
     int filasProcesadas = 0;
     int filasSaltadas = 0;
@@ -26,16 +34,20 @@ int main() {
     while (leyendo && filasProcesadas < limiteFilas) {
         try {
             // Intentamos leer la fila
-            leyendo = in.read_row(user_screen_name);
+            //leyendo = in.read_row(user_screen_name);
+            leyendo = in.read_row(user_id);
             
             // Si la lectura fue exitosa y aun quedan lineaws por leer
             if (leyendo) {
-                int conteo_actual = tablaTweets.get(user_screen_name);
+                //int conteo_actual = tabla_user_screen_name.get(user_screen_name);
+                int conteo_actual = tabla_user_id.get(user_id);
 
                 if (conteo_actual != -1) {
-                    tablaTweets.insertar(user_screen_name, conteo_actual + 1);
+                    //tabla_user_screen_name.insertar(user_screen_name, conteo_actual + 1);
+                    tabla_user_id.insertar(user_id, conteo_actual + 1);
                 } else {
-                    tablaTweets.insertar(user_screen_name, 1);
+                    //tabla_user_screen_name.insertar(user_screen_name, 1);
+                    tabla_user_id.insertar(user_id, 1);
                 }
                 
                 filasProcesadas++; // Solo contamos las filas validas
@@ -48,7 +60,8 @@ int main() {
         }
     }
 
-    tablaTweets.imprimir();
+    //tabla_user_screen_name.imprimir();
+    tabla_user_id.imprimir();
 
     cout << "\nLectura finalizada." << endl;
     cout << "- Filas procesadas correctamente: " << filasProcesadas << endl;
