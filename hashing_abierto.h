ec9ba7c68914a81ee29efa3a1bd0335a18a1fa18
marquinhos03 <mliguempibozzano@gmail.com>
2026-07-04 02:17:00 -0400

    refactor: hashing_abierto es clase template

---
+++ hashing_abierto.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <list>
+
+template <typename CType>
+struct Entrada {
+    CType clave;
+    int valor;
+
+    Entrada(CType c, int v) : clave(c), valor(v) {}
+};
+
+template <typename CType>
+class TablaHashingAbierto {
+private:
+    std::vector<std::list<Entrada<CType>>> tabla;
+    int size;
+
+    int funcionHash(CType clave) const;
+
+public:
+    TablaHashingAbierto(int s) : size(s) {
+        tabla.resize(size);
+    }
+
+    void insertar(CType clave, int valor) {
+        int indice = funcionHash(clave);
+
+        // Si la clave ya existe, actualizamos el valor
+        for (auto& entrada : tabla[indice]) {
+            if (entrada.clave == clave) {
+                entrada.valor = valor;
+                return;
+            }
+        }
+
+        // Si no existe, la agregamos al final
+        tabla[indice].emplace_back(clave, valor);
+    }
+
+    bool get(CType clave, int& resultado) const {
+        int indice = funcionHash(clave);
+
+        for (const auto& entrada : tabla[indice]) {
+            if (entrada.clave == clave) {
+                resultado = entrada.valor;
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    void remover(CType clave) {
+        int indice = funcionHash(clave);
+
+        for (auto it = tabla[indice].begin(); it != tabla[indice].end(); ++it) {
+            if (it->clave == clave) {
+                tabla[indice].erase(it);
+                return;
+            }
+        }
+    }
+    
+};
+
+// --- Función Hash para string (user_screen_name) ---
+
+template <>
+int TablaHashingAbierto<std::string>::funcionHash(std::string clave) const {
+    long long h = 0;
+    int a = 127;
+    for (char v : clave) {
+        h = (a * h + v) % size;
+    }
+    return h;
+}
+
+// --- Función Hash para long long (user_id) ---
+
+template <>
+int TablaHashingAbierto<long long>::funcionHash(long long clave) const {
+    // Para números enteros, aplicamos la división modular directa
+    return clave % size;
+}
